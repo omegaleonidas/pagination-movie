2888563e6facd65470110eebdf190f0fa720917f
sidiq <omegaleonidas33@gmail.com>
2020-09-10 14:22:57 +0700

    proses

--- app/src/main/java/com/example/movie/Adapter/AdapterMovie.kt
+++ app/src/main/java/com/example/movie/Adapter/AdapterMovie.kt
@@ -1,12 +1,19 @@
 package com.example.movie.Adapter
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.Nullable
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.movie.Model.ResultsItem
 import com.example.movie.R
 import com.squareup.picasso.Picasso
@@ -34,13 +41,44 @@ class AdapterMovie :
 //                     Picasso.get()
 //                .load("https://image.tmdb.org/t/p/w500${item?.posterPath}")
 //                .into(itemView.image)
+//
+//
+//            Glide.with(itemView.context).load("https://image.tmdb.org/t/p/w500${item?.posterPath}")
+//                .centerCrop()
+//                .listener(new RequestListener<Drawable>()
+//
+//                .placeholder(R.drawable.prooses)
+//                .into(itemView.image)
 
+            Glide.with(itemView.context)
+                .load("https://image.tmdb.org/t/p/w500${item?.posterPath}")
 
-            Glide.with(itemView.context).load("https://image.tmdb.org/t/p/w500${item?.posterPath}")
-                .centerCrop()
+                .listener(object : RequestListener<Drawable?> {
+                    override fun onLoadFailed(
+                        @Nullable e: GlideException?,
+                        model: Any,
+                        target: Target<Drawable?>,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        itemView.pbLoading.setVisibility(View.GONE)
 
-                .placeholder(R.drawable.prooses)
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any,
+                        target: Target<Drawable?>,
+                        dataSource: DataSource,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        itemView.pbLoading.setVisibility(View.GONE)
+                        return false
+                    }
+                })
                 .into(itemView.image)
+
+
 
         }
 
--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.movie
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.GridLayout
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.movie.Adapter.AdapterMovie
 import com.example.movie.ViewModel.MovieViewModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
 
